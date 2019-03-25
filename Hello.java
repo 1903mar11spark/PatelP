85b3612607271aab2091b71f5b3038d5e29e0d63
Prutha Patel <pruthapatel@Pruthas-MacBook-Air.local>
2019-03-25 12:35:17 -0400

    week B challenge part 2

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package com.weekB;
+
+public class Hello extends FizzBuzz {
+
+public static void main(String[] args) {
+	fizzBuzz1();
+	fizzBuzz2(0,5);
+	fizzBuzz3(1,6,2,3);
+	int myArray [] = {3,4};
+	String otherArray [] = {"hi", "hey"};
+	fizzBuzz4(1,6,myArray,otherArray);
+	}	
+}
+
